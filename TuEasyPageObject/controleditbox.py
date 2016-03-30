b13f1e4f2740d4d39dd0f4ce83c4508f5b73b52d
xugy <hbuxgy@126.com>
2016-03-30 16:34:35 +0800

    xx

--- TuEasyPageObject/controleditbox.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-
-class ControlEditBoxPage(object):
-
-    def __init__(self, driver):
-        self.driver = driver
--- TuEasyPageObject/grapheditbox.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-
-class GraphEditBoxPage(object):
-
-    def __init__(self, driver):
-        self.driver = driver
